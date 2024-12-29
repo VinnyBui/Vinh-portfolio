b0556249cf7dfc3d26e4fea056fef85c65f7be09
Vinh B <vinh.bui0101@gmail.com>
2024-12-29 15:38:31 -0800

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+  <div className="bg-blue-500 text-white p-4">
+    Tailwind is working!asdasd
+  </div>
+
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', // Ensure index.html is included
+    './src/**/*.{js,ts,jsx,tsx}', // Include all relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

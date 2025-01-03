cb9d45af82844897ee7d6d70b5c4555aa430681a
Vinh <vinh.bui0101@gmail.com>
2025-01-03 15:43:22 -0800

    fix width

--- src/components/utils/hero.jsx
+++ src/components/utils/hero.jsx
@@ -4,20 +4,21 @@ import bg from "@/assets/bg.jpg";
 
 const Hero = () => {
     return (
-    <motion.section 
-        className="relative min-h-screen flex flex-col justify-center items-center bg-cover bg-center"
-        style={{ backgroundImage: `url(${bg})` }}
-        animate={{
-            backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
-        }}
-        transition={{
-            duration: 20,
-            repeat: Infinity,
-            ease: "easeInOut",
-        }}
-    >
-        <h1 className="text-white text-5xl">Vinh Bui</h1>
-    </motion.section>
+        <motion.section 
+            className="relative min-h-screen w-full flex flex-col justify-center items-center bg-cover bg-center"
+            style={{ backgroundImage: `url(${bg})` }}
+            animate={{
+                backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
+            }}
+            transition={{
+                duration: 20,
+                repeat: Infinity,
+                ease: "easeInOut",
+            }}
+        >
+            <h1 className="text-white text-5xl">Vinh Bui</h1>
+        </motion.section>
+
     )
 }
 

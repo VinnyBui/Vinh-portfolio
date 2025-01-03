2884ee79d9139d8a03db18c8ce805acab1b62a38
Vinh <vinh.bui0101@gmail.com>
2025-01-03 15:35:35 -0800

    remove extra section

--- src/components/utils/hero.jsx
+++ src/components/utils/hero.jsx
@@ -4,28 +4,20 @@ import bg from "@/assets/bg.jpg";
 
 const Hero = () => {
     return (
-        <section 
-            className="relative min-h-screen w-full mx-auto md:h-screen md:block flex flex-col justify-end items-start bg-cover bg-center" 
-            style={{ backgroundImage: `url(${bg})` }}
-            aria-label="Hero Section"
-        >
-        <motion.section 
-            className="relative min-h-screen flex flex-col justify-center items-center bg-cover bg-center"
-            style={{ backgroundImage: `url(${bg})` }}
-            animate={{
-                backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
-            }}
-            transition={{
-                duration: 20,
-                repeat: Infinity,
-                ease: "easeInOut",
-            }}
-        >
-            <h1 className="text-white text-5xl">Vinh Bui</h1>
-        </motion.section>
-
-
-        </section>
+    <motion.section 
+        className="relative min-h-screen flex flex-col justify-center items-center bg-cover bg-center"
+        style={{ backgroundImage: `url(${bg})` }}
+        animate={{
+            backgroundPosition: ["0% 0%", "100% 100%", "0% 0%"],
+        }}
+        transition={{
+            duration: 20,
+            repeat: Infinity,
+            ease: "easeInOut",
+        }}
+    >
+        <h1 className="text-white text-5xl">Vinh Bui</h1>
+    </motion.section>
     )
 }
 

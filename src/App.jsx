2280af17ec1bf0bc037ac1c9f3ca171fd8ed27b3
Vinh B <vinh.bui0101@gmail.com>
2025-01-07 00:43:30 -0800

    added darkmode as default

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 
 const Home = lazy(() => import('./components/pages/home'));
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <ThemeProvider defaultTheme="dark">
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </ThemeProvider>
   )
 }
 

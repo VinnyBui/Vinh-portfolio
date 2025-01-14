1baf94a18894a520fd16f48cf022ac310fcaa3cd
Vinh B <vinh.bui0101@gmail.com>
2025-01-13 22:59:51 -0800

    adjust sizing to fit screen

--- src/components/utils/hero.jsx
+++ src/components/utils/hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import bg from "@/assets/bg.jpg";
 const Hero = () => {
     return (
-    <section className="h-screen w-screen overflow-hidden">
+    <section className="h-screen w-screen overflow-hidden flex flex-col">
         <div
             className="bg-cover bg-center h-2/5 m-24  border border-white rounded-md"
             style={{ backgroundImage: `url(${bg})` }}
@@ -12,11 +12,11 @@ const Hero = () => {
             </h1>
         </div>
         {/* BIO */}
-        <div className="m-24 flex justify-between">
-            <div>
-                <h1>Vinh Bui</h1>
+        <div className="flex-grow mx-24 mb-24 flex justify-between">
+            <div className="bg-red-500 w-1/2">
+                <h1 className='text-black'>Vinh Bui</h1>
             </div>
-            <div>
+            <div className="bg-blue-500 w-1/2">
                 <p>Software developer looking for work</p>
             </div>
         </div>
